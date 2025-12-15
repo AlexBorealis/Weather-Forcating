c29934e5ea540f011a9676c947894176a05abbd7
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-12-15 21:23:49 +0700

    features, update: написал функционал для открытия и сохранения данных в общий датасет

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,23 +1,42 @@
-import xarray as xr
 import os
+import zipfile
 
-ds = xr.open_dataset(
-    os.path.join(
-        "~/Documents/study/Netology/dll/diploma_project_dll/Weather-Forcating",
-        "data/raw/reanalysis-era5-single-levels/data_stream-oper_stepType-instant.nc",
-    ),
-)
+import xarray as xr
+from dotenv import load_dotenv
 
-print(ds)
+load_dotenv()
 
-print(ds["t2m"])  # Температура на 2м
-print(ds["u10"])  # Ветер (U-компонента)
+DATA_DIR = os.path.join(os.getenv("PROJECT_DIR"), "data", "raw")
+EXTRACTED_DIR = os.path.join(os.getenv("PROJECT_DIR"), "data", "processed", "extracted")
 
-print(ds["t2m"].mean().compute())
-print(ds["t2m"].std().compute())
+archives = os.listdir(DATA_DIR)
+archive_names = [arch.replace(".zip", "") for arch in archives]
+archives = [os.path.join(DATA_DIR, arch) for arch in archives]
+extracted_dirs = [os.path.join(EXTRACTED_DIR, arch) for arch in archive_names]
 
-print(
-    ds["t2m"].sel(
-        valid_time="2025-08-09", latitude=39.75, longitude=-9.75, method="nearest"
-    )
-)
+[os.makedirs(ext, exist_ok=True) for ext in extracted_dirs]
+
+if len(extracted_dirs) == 0 and len(os.listdir(extracted_dirs[-1])) == 0:
+    for arch, ext in zip(archives, extracted_dirs):
+        with zipfile.ZipFile(arch, "r") as zip_ref:
+            zip_ref.extractall(ext)
+
+# last reanalis -----------------------------------------------------------------------
+# u10m, v10m, t2m, sst, sp, skt
+file_name1 = os.path.join(extracted_dirs[-1], os.listdir(extracted_dirs[-1])[0])
+# tp
+file_name2 = os.path.join(extracted_dirs[-1], os.listdir(extracted_dirs[-1])[1])
+
+print(file_name1, file_name2)
+if not os.path.exists(os.path.join(os.path.dirname(file_name1), "all_data.nc")):
+    ds1 = xr.open_dataset(file_name1)
+    ds2 = xr.open_dataset(file_name2)
+
+    if ds1.sizes == ds2.sizes:
+        ds_merged = ds1.merge(ds2, compat="override")
+        ds_merged.to_netcdf(os.path.join(os.path.dirname(file_name1), "all_data.nc"))
+
+ds_main = xr.open_dataset(os.path.join(os.path.dirname(file_name1), "all_data.nc"))
+
+print(ds_main.info())
+print(ds_main.data_vars)
